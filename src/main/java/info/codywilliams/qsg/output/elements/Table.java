6dda7f1e316e30acb1025f8b84c21d57900e8fa5
Cody Williams <williams.c@gmail.com>
2023-07-16 20:31:33 -0500

    Missing >

--- src/main/java/info/codywilliams/qsg/output/elements/Table.java
+++ src/main/java/info/codywilliams/qsg/output/elements/Table.java
@@ -51,7 +51,7 @@ public class Table extends ElementChildren<Table.Row> {
     protected StringBuilder openHtmlTag(StringBuilder stringBuilder, int tabs) {
         super.openHtmlTag(stringBuilder, tabs);
         appendNewLineAndTabs(stringBuilder, tabs);
-        stringBuilder.append("<caption>").append(caption).append("</caption");
+        stringBuilder.append("<caption>").append(caption).append("</caption>");
         return stringBuilder;
     }
 
